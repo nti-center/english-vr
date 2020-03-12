4129ba6843117d1b4f39fc942179a7547778f637
Ilya Zuev <ilyazuev133@gmail.com>
2020-03-12 18:36:42 +1000

    Add scene component for recognition audio with yandex api

--- Source/EnglishVR/EnglishVR.Build.cs
+++ Source/EnglishVR/EnglishVR.Build.cs
@@ -8,7 +8,7 @@ public class EnglishVR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "Http", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
