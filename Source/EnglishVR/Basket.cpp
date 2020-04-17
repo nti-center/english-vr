22f04474343c4fb1db984b7fe6e24b86cd87ff4d
Ilya Zuev <ilyazuev133@gmail.com>
2020-04-17 11:23:44 +1000

    Add fruit added and fruit removed events

--- Source/EnglishVR/Basket.cpp
+++ Source/EnglishVR/Basket.cpp
@@ -79,6 +79,8 @@ void ABasket::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherA
     ++AllFruitCounts[Type];
     //UE_LOG(LogTemp, Warning, TEXT("2 wrong"));
     //OtherActor->Destroy();    
+
+    OnFruitAdded.Broadcast();
 }
 
 void ABasket::OnOutlineOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
@@ -117,6 +119,8 @@ void ABasket::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherAct
     if ((AllFruitCounts.Contains(Type)) && (AllFruitCounts[Type] == 0 )) {
         AllFruitCounts.Remove(Type);
     }
+
+    OnFruitRemoved.Broadcast();
 }
 
 void ABasket::OnOutlineOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
