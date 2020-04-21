01516cea01f4b0f7b1ac32c7f825613e8854e067
Ilya Zuev <ilyazuev133@gmail.com>
2020-04-21 23:55:15 +1000

    Add microsoft cognitive services speech package

--- Source/EnglishVR/EnglishVR.Build.cs
+++ Source/EnglishVR/EnglishVR.Build.cs
@@ -9,13 +9,19 @@ public class EnglishVR : ModuleRules {
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "Http", "Json", "JsonUtilities" });
     
         PrivateDependencyModuleNames.AddRange(new string[] {  });
-    
+
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-        
+
         // Uncomment if you are using online features
         // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-    
+
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        bEnableExceptions = true;
+
+        PublicAdditionalLibraries.Add("$(ProjectDir)/packages/Microsoft.CognitiveServices.Speech.1.11.0/build/native/x64/Release/Microsoft.CognitiveServices.Speech.core.lib");
+        PublicIncludePaths.Add("../packages/Microsoft.CognitiveServices.Speech.1.11.0/build/native/include/cxx_api");
+        PublicIncludePaths.Add("../packages/Microsoft.CognitiveServices.Speech.1.11.0/build/native/include/c_api");
     }
 }
