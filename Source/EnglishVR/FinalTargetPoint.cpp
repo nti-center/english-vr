2853850be89766712788576b650223dde6832945
whiteaangel <assassins95@mail.ru>
2020-05-06 17:37:37 +1000

    Remove log

--- Source/EnglishVR/FinalTargetPoint.cpp
+++ Source/EnglishVR/FinalTargetPoint.cpp
@@ -14,9 +14,7 @@ AFinalTargetPoint::AFinalTargetPoint() {
 void AFinalTargetPoint::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
     if (OtherActor == nullptr || OtherActor == this || OtherComp == nullptr)
         return;
-    
-    UE_LOG(LogTemp, Warning, TEXT("Trigger"));
-    
+
     if (OtherActor && OtherActor != this) {
         Character = Cast<AMyCharacter>(OtherActor);
         if (Character) {
