49dbc726cc3d2bb7d28620bc27896596d736093e
whiteaangel <assassins95@mail.ru>
2020-05-20 17:57:25 +1000

    Add new c++ class for small widget

---
+++ Source/EnglishVR/HandMenuWidget.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HandMenuWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Components/Border.h"
+#include "Components/CanvasPanel.h"
+
+void UHandMenuWidget::NativeConstruct() {
+    Super::NativeConstruct();
+}
+
+bool UHandMenuWidget::Initialize() {
+    bool success = Super::Initialize();
+    if (!success)  return false;
+
+    if (!ensure(Button1 != nullptr)) return false;
+    Button1->OnClicked.AddDynamic(this, &UHandMenuWidget::Button1Clicked);
+
+    if (!ensure(Button2 != nullptr)) return false;
+    Button2->OnClicked.AddDynamic(this, &UHandMenuWidget::Button2Clicked);
+
+    if (!ensure(Button3 != nullptr)) return false;
+    Button3->OnClicked.AddDynamic(this, &UHandMenuWidget::Button3Clicked);
+
+    if (!ensure(Button4 != nullptr)) return false;
+    Button4->OnClicked.AddDynamic(this, &UHandMenuWidget::Button4Clicked);
+
+    return true;
+}
+
+void UHandMenuWidget::Button1Clicked() {
+    LoadLevel("StartMenu");
+}
+
+void UHandMenuWidget::Button2Clicked() {
+    UKismetSystemLibrary::QuitGame(GetWorld(), 0, EQuitPreference::Quit,false);
+}
+
+void UHandMenuWidget::Button3Clicked() {
+    ChangeCanvas(CanvasPanel1, CanvasPanel2);
+}
+
+void UHandMenuWidget::Button4Clicked() {
+    ChangeCanvas(CanvasPanel2, CanvasPanel1);
+}
+
+void UHandMenuWidget::ChangeCanvas(UCanvasPanel* A, UCanvasPanel* B) {
+    A->SetRenderOpacity(0);
+    B->SetRenderOpacity(1);
+}
+
+void UHandMenuWidget::LoadLevel(FString LevelName) {
+    FString CurrentLevel = UGameplayStatics::GetCurrentLevelName(GetWorld());
+    if (LevelName == CurrentLevel)
+        return;
+    &IXRLoadingScreen::ShowLoadingScreen;
+    UGameplayStatics::OpenLevel(GetWorld(), FName(*LevelName), true);
+
+}
---
+++ Source/EnglishVR/HandMenuWidget.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "IXRLoadingScreen.h"
+#include "HandMenuWidget.generated.h"
+
+UCLASS()
+class  UHandMenuWidget : public UUserWidget {
+	GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UCanvasPanel* CanvasPanel1 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UButton* Button1 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UTextBlock* Button1Text = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UButton* Button2 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UTextBlock* Button2Text = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UButton* Button3 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UTextBlock* Button3Text = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UCanvasPanel* CanvasPanel2 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UButton* Button4 = nullptr;
+
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+    class UTextBlock* Button4Text = nullptr;
+
+    UFUNCTION(BlueprintCallable)
+    void Button1Clicked();
+
+    UFUNCTION(BlueprintCallable)
+    void Button2Clicked();
+
+    UFUNCTION(BlueprintCallable)
+    void Button3Clicked();
+
+    UFUNCTION(BlueprintCallable)
+    void Button4Clicked();
+
+    UFUNCTION(BlueprintCallable)
+    void ChangeCanvas(UCanvasPanel* A, UCanvasPanel* B);
+
+    UFUNCTION(BlueprintCallable)
+    void LoadLevel(FString LevelName);
+
+
+protected:
+    virtual bool Initialize();
+
+	
+};
